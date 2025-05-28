b2095247db5f4a6447d7323167691bca2350c647
welty <ijw02012@mymail.pomona.edu>
2025-05-28 10:42:40 -0600

    Drop wmic

--- build_win.bat
+++ build_win.bat
@@ -1,10 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Get number of logical processors for parallel builds
-for /f "tokens=2 delims==" %%i in ('wmic cpu get NumberOfLogicalProcessors /value') do set NUM_CORES=%%i
-
-echo Detected %NUM_CORES% logical processors for parallel building
+:: Detect logical processors
+set "NUM_CORES=%NUMBER_OF_PROCESSORS%"
+if not defined NUM_CORES (
+    for /f %%i in ('powershell -NoProfile -Command ^
+        "(Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors"') ^
+        do set "NUM_CORES=%%i"
+)
+echo Detected !NUM_CORES! logical processors for parallel building
 
 :: Configure build
 cmake -B build-windows ^
@@ -14,9 +18,9 @@ cmake -B build-windows ^
 
 :: Build all targets
 echo Building VST3 plugin...
-cmake --build build-windows --config Release --target Signalbash_VST3 -j%NUM_CORES%
+cmake --build build-windows --config Release --target Signalbash_VST3 -j!NUM_CORES!
 
 echo Building CLAP plugin...
-cmake --build build-windows --config Release --target Signalbash_CLAP -j%NUM_CORES%
+cmake --build build-windows --config Release --target Signalbash_CLAP -j!NUM_CORES!
 
 echo Build complete!
